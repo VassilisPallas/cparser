ba48876e03a7d4c7f4e2a1de84536d32a1fd9061
Vasilis Pallas <vspallas@gmail.com>
2015-03-21 21:38:34 +0200

    a test program in C

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{	
+   /* ektiposi pinaka a[]*/
+   int i, a[]={3,6,7,11,-1,0,1,3,9,1};
+   for(i=0;i<10;i++) /*{*/
+   {
+      printf("%i ",a[i]);
+   }
+   printf("}\n");
+   return 0;
+}
